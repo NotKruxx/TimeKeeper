18743c35db4baf7cf758a1d583a37e236335c6b3
NotKruxx <NotKruxx@users.noreply.github.com>
2026-04-04 15:25:54 +0000

    deploy: 64c6532696e0bf7242c90ca7317b581329ef8f3a

---
+++ drift_worker.js
@@ -0,0 +1,4 @@
+// web/drift_worker.js
+// This file is required for drift to work on the web.
+// It bootstraps the drift worker from a CDN.
+importScripts('https://unpkg.com/@drift_dev/web@2.20.0/drift_worker.js');
